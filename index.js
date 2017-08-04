9cb350e03300b44cf975d6255b8d877b2b5f5576
Sascha Ißbrücker <sascha.issbruecker@byteagenten.de>
2017-08-04 17:04:49 +0200

    Add color display and loading spinner

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
+const ora = require('ora');
 const updater = require('./lib/updater');
 const store = require('./lib/store');
 const calculator = require('./lib/calculator');
-const config = require('./lib/config');
+const display = require('./lib/display');
+
+const spinner = ora();
+spinner.color = 'magenta';
 
 store
     .init()
+    .then(() => spinner.start('Loading time entries...'))
     .then(updater.update)
+    .then(() => spinner.stop())
     .then(() => {
         const totalSummary = calculator.getTotalSummary();
         const weekSummary = calculator.getWeekSummary();
 
-        const totalDiffHours = (totalSummary.difference / 1000 / 60 / 60).toFixed(1);
-        const totalDiffDays = (totalSummary.difference / config.workPerDay).toFixed(1);
-
-        const weekDiffHours = (weekSummary.difference / 1000 / 60 / 60).toFixed(1);
-        const weekDiffDays = (weekSummary.difference / config.workPerDay).toFixed(1);
-
-        console.log(`Total (${totalDiffHours}h, ${totalDiffDays}d)`);
-        console.log(`Week (${weekDiffHours}h, ${weekDiffDays}d)`);
+        display.displaySummary(totalSummary, weekSummary);
     });
