76374c831ca9fc03d083ccdf9c1bb7d7dbcc2f6b
Sascha Ißbrücker <sascha.issbruecker@byteagenten.de>
2017-08-05 18:05:59 +0200

    Implement CLI

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
 #!/usr/bin/env node
-const ora = require('ora');
-const updater = require('./lib/updater');
-const store = require('./lib/store');
-const calculator = require('./lib/calculator');
-const display = require('./lib/display');
-
-const spinner = ora();
-spinner.color = 'magenta';
-
-store
-    .init()
-    .then(() => spinner.start('Loading time entries...'))
-    .then(updater.update)
-    .then(() => spinner.stop())
-    .then(() => {
-        const totalSummary = calculator.getTotalSummary();
-        const weekSummary = calculator.getWeekSummary();
-
-        display.displaySummary(totalSummary, weekSummary);
-    });
+require('./lib/cli');
